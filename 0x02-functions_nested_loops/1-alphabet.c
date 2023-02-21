b0feb933560a0947c9d30354f1859cf1f14af891
salami kabiru <kabirusalami41@gmail.com>
2023-02-21 12:21:52 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * print_alphabet - function prints alphabet in lower case
+ * Return: Always 0.
+ */
+
+
+
+void print_alphabet(void)
+{
+	char u;
+	for (u = 'a' ; u <= 'z' ; u++)
+		_putchar(u);
+	_putchar('\n');
+}
