ea442280c20ba623cdaf70d91787bdf6e1cbf575
salami kabiru <kabirusalami41@gmail.com>
2023-02-21 08:55:00 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,15 @@
  * Return: Always 0.
  */
 int main(void)
-{
-    printf("_putchar\n");
+  {
+    int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int t, u;
+
+	u = sizeof(str) / sizeof(int);
+	for (t = 0; t < u; t++)
+	{
+		putchar(str[count]);
+	}
+	putchar('\n');
     return (0);
 }
